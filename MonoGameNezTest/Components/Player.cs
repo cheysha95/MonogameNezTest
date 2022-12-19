615dd6a1e3ac4c61de76ac529f79a1cd2b93dd74
chey <hyman.sha95@gmail.com>
2022-12-19 16:12:50 -0600

    init commit

---
+++ MonoGameNezTest/Components/Player.cs
@@ -0,0 +1,22 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace MonoGameNezTest;
+
+public class Player : Component
+{
+
+    public Player()
+    {
+
+    }
+
+
+
+
+
+
+}
+
--- MonoGameNezTest/Game1.cs
+++ MonoGameNezTest/Game1.cs
@@ -15,18 +15,28 @@ public class Game1 : Nez.Core
 
     protected override void Initialize()
     {
+
         // TODO: Add your initialization logic here
+        base.Initialize();
         var testScene = new Scene();
+        Core.Scene = testScene;
+
+        //Map
         var MapEntity = testScene.CreateEntity("MapEntity");
-        MapEntity.AddComponent<TiledMapRenderer>("");
+        MapEntity.AddComponent<TiledMapRenderer>(new TiledMapRenderer(Content.LoadTiledMap("Maps\\SimpleMap.tmx")));
+        MapEntity.GetComponent<TiledMapRenderer>().RenderLayer = 1;
+
+        //Player
+        var Player = testScene.CreateEntity("Player");
+        Player.AddComponent<Player>();
+
         
         
-        testScene.ge
-        
+ 
         
         
 
-        base.Initialize();
+
     }
 
   
